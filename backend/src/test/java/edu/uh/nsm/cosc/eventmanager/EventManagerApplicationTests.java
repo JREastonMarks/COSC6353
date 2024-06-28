814b0704a367b97a7e88aa4adade693441ccad5a
Jeremy R Easton-Marks <j.r.eastonmarks@gmail.com>
2024-06-28 09:07:16 -0500

    Initial Setup of Backend

---
+++ backend/src/test/java/edu/uh/nsm/cosc/eventmanager/EventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uh.nsm.cosc.eventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
