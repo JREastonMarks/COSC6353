94852c786f5d4a0489059a90ffb9a3ed4684a4ab
Jeremy R Easton-Marks <j.r.eastonmarks@gmail.com>
2024-08-09 14:08:07 -0500

    Final Tests
    Add more tests

--- backend/src/test/java/edu/uh/nsm/cosc/eventmanager/service/ReportServiceIntegrationTest.java
+++ backend/src/test/java/edu/uh/nsm/cosc/eventmanager/service/ReportServiceIntegrationTest.java
@@ -2,9 +2,13 @@ package edu.uh.nsm.cosc.eventmanager.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,6 +38,18 @@ public class ReportServiceIntegrationTest {
 	void testGenerateEventPDFReportStream() throws IOException {
 		BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("report.pdf"));
 		reportService.generateEventPDFReportStream(out, 1);
+	}
+	
+	@Test
+	void testGenerateVolunteerCSVReportStream() throws IOException {
+		 Writer out = new BufferedWriter(new OutputStreamWriter(System.out));
+		reportService.generateVolunteerCSVReportStream(out, 1);
+	}
+	
+	@Test
+	void testGenerateEventCSVReportStream() throws IOException {
+		Writer out = new BufferedWriter(new OutputStreamWriter(System.out));
+		reportService.generateEventCSVReportStream(out, 1);
 	}
 
 }
